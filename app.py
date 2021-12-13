cd68500cca382bff10e08fe053489869e0b7128d
Nick Volkmann <NickVolkmann>
2021-12-13 20:19:04 +0100

    first initial

--- app.py
+++ app.py
@@ -1,19 +1,70 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from re import M
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from chat import ree
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask_session import Session
+
+
 
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'secret'
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+socketio = SocketIO(app, manage_session=False)
 
-@app.route("/", methods=['GET', 'POST'])
-def login():
-    user_input =""
-    if request.method == "POST":
-        user_input = request.form['user_input'].lower()
-        print(user_input)
-        bot_response = ree(user_input)
-        return render_template("index.html", bot_response=bot_response) 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+    if(request.method=='POST'):
+        username = request.form['username']
+        room = "username"
+        #Store the data in session
+        session['username'] = username
+        session['room'] = room
+        return render_template('chat.html', session = session)
     else:
-        return render_template("Login.html")
+        if session.get('username') is not None:
+            return render_template('chat.html', session = session)
+        else:
+            return redirect(url_for('index'))
 
+def chatbot(message):
+    bot_response = ree(message)
+    print(bot_response)
+    return bot_response
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@socketio.on('join', namespace='/chat')
+def join(message):
+    room = "username"
+    join_room(room)
+    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
+
+@socketio.on('text', namespace='/chat')
+def text(message):
+    print(message['msg'])
+    chatbot(message['msg'])
+    room = "username"
+    emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
+
+@socketio.on('text', namespace='/chat')
+def text(message):
+    print(message['msg'])
+    chatbot(message['msg'])
+    room = "username"
+    emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
+    emit('mess', {'msg': "Chatbot" + ' : ' + chatbot(message['msg'])}, room=room)
+
+@socketio.on('left', namespace='/chat')
+def left(message):
+    room = session.get('room')
+    username = session.get('username')
+    leave_room(room)
+    session.clear()
+    emit('status', {'msg': username + ' has left the room.'}, room=room)
+
+if __name__ == '__main__':
+    socketio.run(app)
